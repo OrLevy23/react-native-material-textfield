72413b2560090d6a48a81a43e34c4fe8746b42ac
orlevy <or.levy@tapingo.com>
2020-04-06 13:06:38 +0300

    wip

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -84,9 +84,6 @@ export default class TextField extends PureComponent {
 
     labelOffset: Label.propTypes.offset,
 
-    labelTextStyle: Text.propTypes.style,
-    titleTextStyle: Text.propTypes.style,
-    affixTextStyle: Text.propTypes.style,
 
     tintColor: PropTypes.string,
     textColor: PropTypes.string,
